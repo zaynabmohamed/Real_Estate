ad2ee3ded566ed7f30c6c29b993819eb7f504441
zaynabmohamed <zm2438320@gmail.com>
2025-12-01 01:18:21 +0200

    updata

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,4 @@
-import Connecting from "./_components/Connecting";
-import FeatureRealestate from "./_components/FeatureRealestate";
-import HeroHome from "./_components/HeroHome";
-import WhatClientWant from "./_components/WhatClientWant";
-import WhatweDo from "./_components/WhatweDo";
 
 
-export default function Home() {
-  return (
-    <div >
-   <HeroHome/>
-   <FeatureRealestate/>
-   <WhatweDo/>
-   <Connecting/>
-   <WhatClientWant />
-    </div>
-  )
-}
+
+
